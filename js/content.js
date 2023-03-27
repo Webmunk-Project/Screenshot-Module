8736b513f1a3fb7544fe6884d0cfccfd5bfd7cc6
Chris Karr <chris@audacious-software.com>
2023-03-27 12:28:00 -0500

    HTML2Canvas initial implementation work

--- js/content.js
+++ js/content.js
@@ -1,77 +1,109 @@
-/* global chrome, locationFilterMatches, ImageCapture, createImageBitmap, OffscreenCanvas */
+/* global chrome, locationFilterMatches, html2canvas */
 
-window.registerModuleCallback(function (config) {
-  // console.log('[Cookies] Checking host...')
-  // console.log(config)
+(function () {
+  window.registerModuleCallback(function (config) {
+    config['screenshot-filters'] = [{
+      hostEquals: 'www.cnn.com'
+    }]
 
-  config['screenshot-filters'] = [{
-    hostEquals: 'cnn.com'
-  }, {
-    hostSuffix: '.cnn.com'
-  }]
+    config['screenshot-rules'] = [{
+      match: '.zn-homepage1-zone-1 article:not(.screenshot-taken)'
+    }]
 
-  config['screenshot-rules'] = [{
-    match: 'article'
-  }]
+    if (locationFilterMatches(window.location, config['screenshot-filters'])) {
+      console.log('[Screenshot] Checking ' + window.location + '.')
 
-  if (locationFilterMatches(window.location, config['screenshot-filters'])) {
-    config['screenshot-rules'].forEach(function (rule) {
-      const matches = $(document).find(rule.match)
+      const pageUpdate = function () {
+        config['screenshot-rules'].forEach(function (rule) {
+          const matches = $(document).find(rule.match)
 
-      matches.each(function (index, element) {
-        const domElement = element // .get(0)
+          console.log('[Screenshot] Rule: ' + rule.match + ': ' + matches.length)
 
-        const rect = domElement.getBoundingClientRect()
+          matches.each(function (index, element) {
+            $(element).addClass('screenshot-taken')
 
-        console.log('[Screenshot] Recording screenshot from ' + window.location + '...')
+            const domElement = element // .get(0)
 
-        const top = rect.top
-        const left = rect.left
-        const width = rect.width
-        const height = rect.height
+            const rect = domElement.getBoundingClientRect()
 
-        console.log('[Screenshot] CAPTURE ' + top + ', ' + left + ', ' + width + ', ' + height)
-        console.log(chrome)
+            console.log('[Screenshot] Recording screenshot from ' + window.location + '...')
 
-        chrome.tabCapture.capture({
-          audio: false,
-          video: true
-        }, function (stream) {
-          const track = stream.getVideoTracks()[0]
-          const imageCapture = new ImageCapture(track)
+            const top = rect.top
+            const left = rect.left
+            const width = rect.width
+            const height = rect.height
 
-          const image = imageCapture.grabFrame()
+            console.log('[Screenshot] CAPTURE ' + top + ', ' + left + ', ' + width + ', ' + height)
+            console.log(chrome)
+            console.log(html2canvas)
 
-          image.then(function (bitmap) {
-            stream.stop()
+            window.setTimeout(function () {
+              html2canvas(element, {
+                allowTaint: false,
+                useCORS: true
+              }).then(function (canvas) {
+                console.log('[Screenshot] CANVAS')
+                console.log(canvas)
 
-            const croppedImage = createImageBitmap(bitmap, left, top, width, height)
+                document.body.appendChild(canvas)
 
-            const offscreen = new OffscreenCanvas(width, height)
-            const renderer = offscreen.getContext('bitmaprenderer')
+                const base64image = canvas.toDataURL('image/png')
+                console.log('base64image')
+                console.log(base64image)
 
-            renderer.transferFromImageBitmap(croppedImage)
+                canvas.remove()
+              })
 
-            const dataUrl = renderer.toDataUrl('image/png')
+              /*
 
-            console.log('[Screenshot] IMAGE URL: ')
-            console.log(dataUrl)
+                chrome.tabCapture.capture({
+                  audio: false,
+                  video: true
+                }, function (stream) {
+                  const track = stream.getVideoTracks()[0]
+                  const imageCapture = new ImageCapture(track)
 
-            chrome.runtime.sendMessage({
-              content: 'record_screenshot',
-              url: window.location.href,
-              image: dataUrl,
-              top,
-              left,
-              width,
-              height,
-              matches: 'todo'
-            })
+                  const image = imageCapture.grabFrame()
+
+                  image.then(function (bitmap) {
+                    stream.stop()
+
+                    const croppedImage = createImageBitmap(bitmap, left, top, width, height)
+
+                    const offscreen = new OffscreenCanvas(width, height)
+                    const renderer = offscreen.getContext('bitmaprenderer')
+
+                    renderer.transferFromImageBitmap(croppedImage)
+
+                    const dataUrl = renderer.toDataUrl('image/png')
+
+                    console.log('[Screenshot] IMAGE URL: ')
+                    console.log(dataUrl)
+
+                    chrome.runtime.sendMessage({
+                      content: 'record_screenshot',
+                      url: window.location.href,
+                      image: dataUrl,
+                      top,
+                      left,
+                      width,
+                      height,
+                      matches: 'todo'
+                    })
+                  })
+                })
+
+              */
+            }, 2500)
           })
         })
-      })
-    })
-  } else {
-    console.log('[Screenshot] Skipping ' + window.location + '.')
-  }
-})
+      }
+
+      window.registerModulePageChangeListener(pageUpdate)
+
+      pageUpdate()
+    } else {
+      console.log('[Screenshot] Skipping ' + window.location + '.')
+    }
+  })
+})(); // eslint-disable-line semi, no-trailing-spaces
--- js/worker.js
+++ js/worker.js
@@ -1,49 +1,53 @@
-/* global chrome, handleMessage, registerCustomModule, registerMessageHandler */
+/* global registerCustomModule, registerMessageHandler */
 
-const recordScreenshot = function (request, sender, sendResponse) {
-  if (request.content === 'take_screenshot') {
-    const top = request.top
-    const left = request.left
-    const width = request.width
-    const height = request.height
+(function () {
+  const recordScreenshot = function (request, sender, sendResponse) {
+    if (request.content === 'take_screenshot') {
+      const top = request.top
+      const left = request.left
+      const width = request.width
+      const height = request.height
 
-    console.log('[Screenshot] Recording screenshot for ' + request.url + ' at region (' + top + ', ' + left + ' - ' + width + 'x' + height + '...')
+      console.log('[Screenshot] TODO Recording screenshot for ' + request.url + ' at region (' + top + ', ' + left + ' - ' + width + 'x' + height + '...')
 
-    const payload = {
-      'url*': request.url,
-      'page-title*': request.pageTitle
+      /*
+            const payload = {
+              'url*': request.url,
+              'page-title*': request.pageTitle
+            }
+
+            chrome.cookies.getAll({
+              url: request.url
+            },
+            function (cookies) {
+              cookies.forEach(function (cookie) {
+                console.log(cookie.name + ' --> ' + cookie.name)
+                console.log(cookie)
+
+                payload.cookies.push(cookie)
+              })
+
+              if (payload.cookies.length > 0) {
+                const newRequest = {
+                  content: 'record_data_point',
+                  generator: 'browser-cookies',
+                  payload: payload // eslint-disable-line object-shorthand
+                }
+
+                handleMessage(newRequest, sender, sendResponse)
+              }
+            })
+        */
+
+      return true
     }
 
-    chrome.cookies.getAll({
-      url: request.url
-    },
-    function (cookies) {
-      cookies.forEach(function (cookie) {
-        console.log(cookie.name + ' --> ' + cookie.name)
-        console.log(cookie)
-
-        payload.cookies.push(cookie)
-      })
-
-      if (payload.cookies.length > 0) {
-        const newRequest = {
-          content: 'record_data_point',
-          generator: 'browser-cookies',
-          payload: payload // eslint-disable-line object-shorthand
-        }
-
-        handleMessage(newRequest, sender, sendResponse)
-      }
-    })
-
-    return true
+    return false
   }
 
-  return false
-}
+  registerCustomModule(function (config) {
+    console.log('[Screenshot] Initialized.')
 
-registerCustomModule(function (config) {
-  console.log('[Screenshot] Initialized.')
-
-  registerMessageHandler('take_screenshot', recordScreenshot)
-})
+    registerMessageHandler('take_screenshot', recordScreenshot)
+  })
+})()
